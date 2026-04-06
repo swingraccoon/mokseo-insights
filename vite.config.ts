81bef3b6b37a644d2c5237d0ea75358fc5f51f64
swingraccoon <swingraccoon@gmail.com>
2026-04-06 16:32:34 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite" // 이 줄이 디자인 엔진입니다
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()], // 여기에 tailwindcss()를 추가했습니다
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
